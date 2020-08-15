c5e1f503cf9daa1d7de6634f7e9c1eeefd3b1100
vindhya Hegde <50870871+vindhyasadanand@users.noreply.github.com>
2020-08-15 11:06:07 +0530

    Triplet with zero sum - only return true or false

--- Arrays/Triplet.java
+++ Arrays/Triplet.java
@@ -1,9 +1,35 @@
+import java.util.Arrays;
 
 public class Triplet {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		int arr[] = {0, -1, 2, -3, 1};
 		// TODO Auto-generated method stub
+		Arrays.sort(arr);
+		for(int i=0;i<arr.length;i++)
+		{
+			int start = i+1;
+			int end = arr.length-1;
+			while(start<end)
+			{
+				if(arr[i]+arr[start]+arr[end]==0)
+				{
+					System.out.println("True");
+					System.exit(0);
+				}
+				else if(arr[i]+arr[start]+arr[end]<0)
+				{
+					start++;
+				}
+				else
+				{
+					end--;
+				}
+			}
+		}
 
 	}
 
 }
+
