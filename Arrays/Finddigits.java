882e4282d0ad3f4537838029540efb344b72a8a9
vindhya Hegde <vindhyasadanand31@gmail.com>
2021-07-03 10:20:50 +0530

    Create Finddigits.java

---
+++ Arrays/Finddigits.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'findDigits' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int findDigits(int n) 
+    {
+        int count = 0;
+        int num = n;
+        while(n!=0)
+        {
+            int cur = n%10;
+            if(cur!=0)
+            {
+                if(num%cur==0)
+                {
+                     count++;
+                }
+            } 
+            n = n/10;
+        }
+      return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+            int result = Result.findDigits(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
