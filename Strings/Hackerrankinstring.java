434d819e00a748fb13db2bac61e9f6d1e35ce4af
vindhya Hegde <vindhyasadanand31@gmail.com>
2021-07-04 06:03:31 +0530

    Hackerrank in a string

---
+++ Strings/Hackerrankinstring.java
@@ -0,0 +1,16 @@
+public static String hackerrankInString(String s) {
+    // Write your code here
+               String str = "hackerrank";
+        if (s.length() < str.length()) {
+            return "NO";
+        }
+        int j = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (j < str.length() && s.charAt(i) == str.charAt(j)) {
+                    j++;
+            }
+        }
+        return (j == str.length() ? "YES" : "NO");
+
+
+    }
